6826961e9184c78e62f9a426150647f241142e61
Diego Batista Gomes Moraes <diegobatistt@gmail>
2025-12-26 16:03:34 -0300

    fix: corrigir vite

--- freelance-platform/vite.config.ts
+++ freelance-platform/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Funciona em qualquer host (Vercel, GitHub Pages, etc)
+  build: {
+    outDir: 'dist',
+  },
 })
